2d67c5433b3810351adbef8e0361e54067609949
Siddharth-afk <66246290+Siddharth-afk@users.noreply.github.com>
2021-07-28 14:49:06 +0530

    added sound files

--- index.js
+++ index.js
@@ -1,2 +1,45 @@
-alert("hellow");
+time = true;
+var list = [];
+var count = 0;
+var green = 0;
+var red = 1;
+var brown = 2;
+var blue = 3;
 
+while(time){
+    var num = Math.floor(Math.random() * 4);
+    list.push(num);
+
+    $("button#" + list[count] + " .block").css("background-color", "red");
+    setInterval(function(){
+        $("button#" + list[count] + " .block").remove();
+    }, 300);
+    count++;
+
+
+    if(count == 2){
+        time = false;
+    }
+}
+
+
+/*$(document).keypress(function(i){
+    $("h1").text(i.key);
+})
+
+$("button#green").click(function(){
+    console.log("a");
+})
+
+$("button#red").click(function(){
+    console.log("b");
+})
+
+$("button#brown").click(function(){
+    console.log("c");
+})
+
+
+$("button#blue").click(function(){
+    console.log("d ");
+})*/
