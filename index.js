b56264b68ef89eb35b15282ae0a3754e9651965c
Siddharth-afk <66246290+Siddharth-afk@users.noreply.github.com>
2021-07-30 23:23:13 +0530

    fixed up more javascript, only thing left is fix it

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
-time = true;
 var gamepattern = [];
+var count = 0;
 var buttonColours  = ["green", "red", "brown", "blue"];
-var randomColourChoosen = buttonColours[nextSequence()];
-gamepattern.push(randomColourChoosen);
+var randomColourChoosen;
+var sad = new Audio("sounds/sad_truth.mp3");
 
-flash(randomColourChoosen);
+$(document).keypress((i) =>{
+    if (i.key == "Enter"){
+        start();
+    }
+});
 
-console.log(randomColourChoosen);
+function start(){
+    count++;
+    var i = 0;
+    $("h1").text("Level " + count);
+    randomColourChoosen = buttonColours[nextSequence()];
+    flash(randomColourChoosen);
+    gamepattern.push(randomColourChoosen);
+    console.log(gamepattern);
+
+    for(i = 0; i < gamepattern.length; i++){      
+        $("div.block").click((event) => {
+            var target;
+            target = event.target["id"];
+            if(check(target, gamepattern, i-1)){
+                start();
+            }
+        });
+    }
+}
 
 function nextSequence(){
     var random_number = Math.floor(Math.random() * 4);
@@ -14,7 +36,20 @@ function nextSequence(){
 }
 
 function flash(i){
-    $("#" + i).fadeOut(100, () => {
+    $("#" + i).fadeOut(300, () => {
         $("#" + i).fadeIn(100);
     });
+}
+
+function playAudio(i){
+    var audio = new Audio("sounds/" + i + ".mp3");
+    audio.play();
+}
+
+function check(event, gamepattern, pos){
+    if(event == gamepattern[pos]){
+        flash(event);
+        playAudio(event);
+        return true;
+    }
 }
